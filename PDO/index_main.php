b7b142fea69cc890aff8eb92fda1f17c57fc759b
compilerharris <shaikhhgr@gmail.com>
2017-08-02 14:34:55 +0530

    Add files via upload

---
+++ PDO/index_main.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+	<?php 
+		//include register and login class
+		include 'register.php';
+		include 'login.php';
+
+		$register = new RegisterFrom();
+		echo "<br><br><br>";
+		$login = new LoginFrom();
+ 	?>
+</body>
+</html> 
