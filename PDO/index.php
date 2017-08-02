2ab5915366820c7913045de6da1cf8c71c43eacf
compilerharris <shaikhhgr@gmail.com>
2017-08-02 14:31:35 +0530

    Add files via upload

--- PDO/index.php
+++ PDO/index.php
@@ -1,16 +1,41 @@
+<?php 
+	try{	
+		$pdo = new PDO("mysql:host=localhost;dbname=erail","root","root");
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}catch(PDOException $e){
+		echo ;
+	}
+
+ ?>
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Home</title>
+	<title>E Rail</title>
 </head>
 <body>
-	<?php 
-		//include register and login class
-		include 'register.php';
-		include 'login.php';
-
-		$register = new RegisterFrom();
-		echo "<br><br><br>";
-		$login = new LoginFrom();
- 	?>
+<div>
+	<form action="" method="post">
+		<label>Source : </label>
+		<select name="sStn">
+			<option name="PL">Panvel</option>
+			<option name="BR">Belapur CBD</option>
+			<option name="VA">Vashi</option>
+			<option name="KH">Khandeshwar</option>
+		</select>
+		<br><br>
+		<label>Destination : </label>
+		<select name="dStn">
+			<option name="PL">Panvel</option>
+			<option name="BR">Belapur CBD</option>
+			<option name="VA">Vashi</option>
+			<option name="KH">Khandeshwar</option>
+		</select>
+		<br><br>
+		<button type="submit">Print</button>
+	</form>
+</div>
 </body>
 </html>
+<?php 
+		
+ ?>
