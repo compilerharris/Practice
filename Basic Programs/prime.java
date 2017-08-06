0c7d1768a4385016ed542a5a476d65f65ba7344d
compilerharris <shaikhhgr@gmail.com>
2017-08-06 21:41:19 +0530

    Update prime.java

--- Basic Programs/prime.java
+++ Basic Programs/prime.java
@@ -5,7 +5,7 @@ class Prime{
 		System.out.println("Enter number:");
 		Scanner sc = new Scanner(System.in);
 		int no = sc.nextInt();
-		int flag = 0, i;
+		int i;
 		for (i = 2 ; i < no ; i += 1 ) {
 			if (no%i == 0) {
 				break;
